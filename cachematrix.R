193a9d0efd2a774f80ae7078f4973ccc26426d4d
eanakhl <sleem@mail.ru>
2015-11-10 22:34:31 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
 ## Caching the Inverse of a Matrix
-## his function computes the inverse of the special "matrix".
-## If the inverse has already been calculated (and the matrix has not changed),
-## then cacheSolve should retrieve the inverse from the cache.
+
 
 ## Creates a special "matrix" object that can cache its inverse.
 
